6b4a2c1192d3a80ec3df5219d0862832dc50acb8
Alberto Morales <cmtmorales@gmail.com>
2019-02-20 15:31:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+var x=[];
+
+
+for(var i=0;i<musicians.length;i++){
+var output=musicians[i]+ ' plays '+instruments[i];
+  x.push(output);
+}
+  
+return x;
+
+  
+  
+}    function johnLennonFacts
